be6b1f952ea17812d501fbc764f3299f7043ae17
daniel <daniel@pop-os.broadband>
2025-03-17 19:15:10 +0000

    Added file list download

--- scraper.py
+++ scraper.py
@@ -1,9 +1,7 @@
 from playwright.async_api import async_playwright
 import asyncio
 import re
-
-url = input('Enter leak site address: ')
-company_name = input('Enter company name: ')
+import os
 
 async def main():
 
@@ -14,7 +12,8 @@ async def main():
             proxy={"server": "socks5://127.0.0.1:9150"}
         )
         context = await browser.new_context(
-            viewport={"width": 1920, "height": 1080}
+            viewport={"width": 1920, "height": 1080},
+            accept_downloads=True
         )
         page = await context.new_page()
 
@@ -29,10 +28,36 @@ async def main():
         await company_detail.wait_for(state="visible")
         await page.wait_for_function("getComputedStyle(document.querySelector('.modal.show')).opacity === '1'")
         bounding_box = await company_detail.bounding_box()
-        await page.screenshot(path="test.png", clip=bounding_box)
+        await page.screenshot(path=f"{company_name}/screenshot.png", clip=bounding_box)
 
+        links = await company_detail.get_by_text("Documents").all()
+        dir_files = []
+        for link in links:
+            dir_files.append(await link.get_attribute("href") + "data/ALL_FILES")
+        print(dir_files)
 
+        counter = 1
+        for href in dir_files:
+            page = await browser.new_page()
+    
+            async with page.expect_download() as download_info:
+                await page.evaluate(f'window.location.href = "{href}"')
+
+            download = await download_info.value  
+            file_path = f"./{company_name}/Documents_{counter}.txt"
+            await download.save_as(file_path)  
+
+            print(f"Download completed: {file_path}")
+    
+            counter += 1
+            await asyncio.sleep(2)        
+            
         await browser.close()
+
+
+url = input('Enter leak site address: ')
+company_name = input('Enter company name: ')
+os.mkdir(company_name)
 
 if __name__ == '__main__':
     asyncio.run(main())
